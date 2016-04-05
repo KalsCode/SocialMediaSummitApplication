e0922308eadad184102323f855451fef5e14820c
KalsCode <anthonykalanda@gmail.com>
2016-04-05 16:18:09 +0300

    Initial setup

---
+++ SocialMediaSummitApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialMediaSummitApplication.MvcApplication" Language="C#" %>
---
+++ SocialMediaSummitApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialMediaSummitApplication.Startup))]
+namespace SocialMediaSummitApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
