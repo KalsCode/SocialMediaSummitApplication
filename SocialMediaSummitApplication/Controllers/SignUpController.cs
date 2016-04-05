b08adca825485f47395ece901fa5316c9a78e3da
KalsCode <anthonykalanda@gmail.com>
2016-04-05 16:34:49 +0300

    SignUp controller

---
+++ SocialMediaSummitApplication/Controllers/SignUpController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SocialMediaSummitApplication.Models;
+
+namespace SocialMediaSummitApplication.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private SignUpDbContext db = new SignUpDbContext();
+
+        // GET: SignUp
+        public ActionResult Index()
+        {
+            return View(db.SignUps.ToList());
+        }
+
+        // GET: SignUp/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SignUp signUp = db.SignUps.Find(id);
+            if (signUp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(signUp);
+        }
+
+        // GET: SignUp/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SignUp/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SignUpId,SignUpCategory,Name,Location,Email,AppId,AppName,AppCategory,AppDesc,HostUrl1,HostUrl2,HostUrl3")] SignUp signUp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SignUps.Add(signUp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(signUp);
+        }
+
+        // GET: SignUp/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SignUp signUp = db.SignUps.Find(id);
+            if (signUp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(signUp);
+        }
+
+        // POST: SignUp/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SignUpId,SignUpCategory,Name,Location,Email,AppId,AppName,AppCategory,AppDesc,HostUrl1,HostUrl2,HostUrl3")] SignUp signUp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(signUp).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(signUp);
+        }
+
+        // GET: SignUp/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SignUp signUp = db.SignUps.Find(id);
+            if (signUp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(signUp);
+        }
+
+        // POST: SignUp/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SignUp signUp = db.SignUps.Find(id);
+            db.SignUps.Remove(signUp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
